52c0bb3a0869b3a80c5096defa4c035ae0c6e457
shinichi-ito <shinichi-ito@lotsjoys.com>
2016-12-13 13:41:58 +0900

    43

---
+++ src/app/highlight.directive.ts
@@ -0,0 +1,21 @@
+import {Directive, ElementRef, Renderer, HostListener, HostBinding} from '@angular/core';
+
+@Directive({
+  selector: '[appHighlight]'
+})
+export class HighlightDirective {
+  @HostListener('mouseenter') mouseover(){
+    this.backgroundColor='green';
+  };
+  @HostListener('mouseleave') mouseleave(){
+    this.backgroundColor='white';
+  };
+  @HostBinding('style.backgroundColor') get setColor(){
+    return this.backgroundColor;
+  };
+  private backgroundColor='white';
+  constructor() {
+    //this.elementRef.nativeElement.style.backgroundColor='green';
+    //this.renderer.setElementStyle(this.elementRef.nativeElement,'background-color','green');
+  }
+}
